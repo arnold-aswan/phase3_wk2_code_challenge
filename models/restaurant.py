f4819d93922d4f0c59a93ffebf1d98fd45f89eee
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-27 20:34:09 +0300

    renamed folder to lowercase

---
+++ models/restaurant.py
@@ -0,0 +1,21 @@
+from models.review import Review
+
+class Restaurant:
+    restaurants = []
+    def __init__(self, name):
+        self.name = name
+        Restaurant.restaurants.append(self)
+        
+    def name(self):
+        return self.name  
+        
+    def reviews(self):
+        return [review for review in Review.reviews if review.restaurant().name() == self.name]    
+    
+    def customers(self):
+        unique_customers = [review.customer() for review in self.reviews()]
+        return list(set(unique_customers))
+    
+    def average_star_rating(self):
+        rating = sum(review.rating() for review in self.reviews())
+        return rating / len(self.reviews())
