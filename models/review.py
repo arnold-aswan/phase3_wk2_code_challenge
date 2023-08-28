90340b27f6a7bb62fcab50883b913f0f482d36c4
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-28 08:15:11 +0300

    added comments to review class methods

--- models/review.py
+++ models/review.py
@@ -8,16 +8,24 @@ class Review:
         Review.reviews.append(self) 
         
     def rating(self):
+        """Returns the rating for the restaurant
+        """
         return self._rating  
     
     @classmethod
     def all(cls):
+        """Returns all of the reviews
+        """
         return cls.reviews
     
     def customer(self):
+        """Returns the customerobject for that review
+        """
         return self._customer
     
     def restaurant(self):
+        """Returns the restaurant object for that review
+        """
         return self._restaurant
           
         
