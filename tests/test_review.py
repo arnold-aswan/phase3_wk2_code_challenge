d97f1e826cc86350c13c7a90205a9b3d6edb9698
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-28 15:57:03 +0300

    added test scenarios to review class

--- tests/test_review.py
+++ tests/test_review.py
@@ -10,6 +10,9 @@ class TestReview:
         restaurant = Restaurant("konoha")
         review = Review(customer, restaurant, rating=5)
         
+        customer3 = Customer("seth", "rollins")
+        restaurant3 = Restaurant("konoha senpo")
+        review3 = Review(customer3, restaurant3, rating=5)
         assert review != None
         assert review.restaurant() == restaurant
         assert review.customer() == customer
@@ -20,10 +23,45 @@ class TestReview:
         """    
         get_review = self.create_review()
         assert get_review.rating() == 4
-        
+     
+    def test_review_all(self):
+        """Returns all of the reviews
+        """
+        assert len(Review.all()) == 7   
             
+    def test_review_customer(self):
+        """Returns customer object
+        """
+        customer = Customer("John", "Doe")
+        restaurant = Restaurant("Limbo")
+        review = Review(customer, restaurant, rating=5)
+        assert customer.full_name() == "John Doe" 
+    
+    def test_review_restaurant(self):
+        """Returns restaurant object
+        """
+        customer = Customer("John", "Doe")
+        restaurant = Restaurant("Limbo")
+        review = Review(customer, restaurant, rating=5)
+        assert restaurant.get_name() == "Limbo" 
+                
+                
+    # def test_average_star_rating(self):
+    #     """Returns average rating
+    #     """
+    #     customer = Customer("John", "Doe")
+    #     restaurant = Restaurant("Limbo")
+    #     review = Review(customer, restaurant, rating=5)
+        
+    #     customer02 = Customer("Jane", "Doe")
+    #     restaurant02 = Restaurant("Limbo")
+    #     review02 = Review(customer02, restaurant02, rating=5)
+        
+    #     assert Restaurant().average_star_rating() == 5
+       
+                    
     def create_review(self):
-        customer = Customer("Alice", "Smith")
-        restaurant = Restaurant("Sample Restaurant")
-        review = Review(customer, restaurant, rating=4)    
-        return review
+        customer1 = Customer("Alice", "Smith")
+        restaurant1 = Restaurant("Los santos")
+        review1 = Review(customer1, restaurant1, rating=4)    
+        return review1
