e0b732a329cacd5469508cd36350d5d10d9ccefb
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-28 08:11:59 +0300

    added comments to restaurant class methods

--- models/restaurant.py
+++ models/restaurant.py
@@ -7,15 +7,23 @@ class Restaurant:
         Restaurant.restaurants.append(self)
         
     def get_name(self):
+        """Returns the restaurant name
+        """
         return self._name  
         
     def reviews(self):
+        """Returns a list of reviews for the restaurant
+        """
         return [review for review in Review.reviews if review.restaurant().name() == self._name]    
     
     def customers(self):
+        """Returns a unique list of customers who have reviewed a restaurant
+        """
         unique_customers = [review.customer() for review in self.reviews()]
         return list(set(unique_customers))
     
     def average_star_rating(self):
+        """Returns average star rating for a restaurant
+        """
         rating = sum(review.rating() for review in self.reviews())
         return rating / len(self.reviews())
